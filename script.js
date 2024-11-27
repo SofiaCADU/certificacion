a6c47c8b784cf261914b73834f4d1e00ac10dad5
SofiaCADU <sofia.caceres.duran@alumnos.sip.cl>
2024-11-27 10:19:39 -0300

    creacion segunda  funcion js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function iSesión(){
     alert("Inicio de Sesión exitos")
 }
+
+document.getElementById("rentar").onclick = function() {
+    this.textContent = "No disponible"
+}
+document.getElementById("renta").onclick = function() {
+    this.textContent = "No disponible"
+}
+document.getElementById("rentas").onclick = function() {
+    this.textContent = "No disponible"
+}
